1c70abe0b7e8418e3d1f81c9e011b0830e24dd69
Withstyle2099 <adool2099@gmail.com>
2026-02-17 21:38:53 +0300

    Fix indentation in load_data function

--- lsi_streamlit_app.py
+++ lsi_streamlit_app.py
@@ -48,8 +48,7 @@ np.random.seed(42)
 # Load data
 @st.cache_data
 def load_data():
-    df = pd.read_csv('AI_LSI_Demo_Historical_Data.csv')
-    return df
+AI_LSI_Demo_Historical_Data.csv    return df
 
 # Train models
 @st.cache_resource
